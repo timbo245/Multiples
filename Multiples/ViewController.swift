1ec748667b980c814845eea6aa64bbb12e9c7350
Timothy Lott <timbo245@yahoo.com>
2015-12-30 10:39:21 -0800

    Initial Commit

---
+++ Multiples/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Multiples
+//
+//  Created by Timothy Lott on 12/30/15.
+//  Copyright © 2015 Timothy M. Lott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
